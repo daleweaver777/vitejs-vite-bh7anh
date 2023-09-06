cb9e8a53fd5408179f8a683923c25fe09f27bfcb
weavdale <68517144+weavdale@users.noreply.github.com>
2023-09-06 17:16:57 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import { h, ref } from 'vue';
+import { renderToString } from 'vue/server-renderer';
+
+const html = ref('');
+
+const htmlFunc = async () => {
+  const mycomp = h(HelloWorld, { name: 'Dale', phone: '777', cell: '111' });
+  const rendered = await renderToString(mycomp);
+  html.value = rendered.replace('<!--[-->', '').replace('<!--]-->', '');
+};
+</script>
+
+<template>
+  <div>
+    <button :onClick="htmlFunc">Get</button>
+    <p>bob</p>
+    <p v-html="html"></p>
+  </div>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  name: String,
+  phone: String,
+  cell: String,
+});
+</script>
+
+<template>
+  <p>Name: {{ name }}</p>
+  <p>Phone: {{ phone }}</p>
+  <p>Cell: {{ cell }}</p>
+</template>
